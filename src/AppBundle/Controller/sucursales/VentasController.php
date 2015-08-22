1f4b39c5c819c18a7a5bda42a5359760d0d66d87
Vladimir Hidalgo <vladimiroski@gmail.com>
2015-08-22 03:45:53 -0600

    Optimizar query de ventas

--- src/AppBundle/Controller/sucursales/VentasController.php
+++ src/AppBundle/Controller/sucursales/VentasController.php
@@ -21,20 +21,14 @@ class VentasController extends Controller {
         
         $em = $this->getDoctrine()->getManager();
 
-        $sql = " 
-            SELECT s.nombre, c.id, c.fecha, c.total, c.total_pendiente, c.total_posible, c.total_anulado, c.total_cancelado, c.total_descuentos, c.total_compras, c.total_cuadrar, c.total_compras_cuadrar, c.sucursal_id
-            FROM sucursales s
-            JOIN empleados_sucursales es
-            ON s.id = es.sucursales_id AND es.empleados_id = :empleados_id
-            LEFT JOIN cortez c ON s.id = c.sucursal_id
-            WHERE c.id = (
-            SELECT c2.id
-            FROM cortez c2
-            WHERE c2.sucursal_id = c.sucursal_id AND
-            c2.fecha <= :fecha
-            ORDER BY c2.id DESC
-            LIMIT 1)
-            ORDER BY s.id ASC
+        $sql = "
+        SELECT s.nombre, c.id, c.fecha, c.total, c.total_pendiente, c.total_posible, c.total_anulado, c.total_cancelado, c.total_descuentos, c.total_compras, c.total_cuadrar, c.total_compras_cuadrar, c.sucursal_id
+        FROM sucursales s
+        JOIN empleados_sucursales es ON s.id = es.sucursales_id AND es.empleados_id = :empleados_id
+        JOIN ( SELECT MAX(st1.id) AS id, st1.sucursal_id, st1.fecha FROM cortez st1 WHERE st1.fecha <= :fecha GROUP BY st1.sucursal_id)  AS c2
+        ON es.sucursales_id = c2.sucursal_id
+        JOIN cortez c ON c.id = c2.id 
+        ORDER BY s.id ASC
         ";
 
         $stmt = $em->getConnection()->prepare($sql);
